def02f7a4345f963ed6756986ddf85367d608920
piyushbartakke <piyushbartakke@outlook.in>
2024-05-09 05:03:11 +0530

    Added edit and delete buttons in Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -6,17 +6,22 @@ namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        //-------------------------------------------------------------------------------------
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db) 
         {
             _db = db;   //basically dependency injection
         }
+
+        //-------------------------------------------------------------------------------------
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
 
+        //-------------------------------------------------------------------------------------
+        //Create action methods
         public IActionResult Create() 
         { 
             return View();
@@ -25,6 +30,7 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj) 
         {
+            //server side validations reduce performance
             //custom validation 1
             if (obj.Name == obj.DisplayOrder.ToString()) 
             {
@@ -45,6 +51,30 @@ namespace BulkyWeb.Controllers
                 return RedirectToAction("Index"); //in different controller: ("Index", "Category")
             }
 
+            return View();
+        }
+
+        //-------------------------------------------------------------------------------------
+        //Edit action methods
+        public IActionResult Edit(int? id) 
+        {
+            if(id == null || id == 0)
+                return NotFound();
+
+            //Fetching from database
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id); --> prefer this as it can be used with non-primary key as well
+            //Category? categroyFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault(); --> when a number of filtering operations is to be done
+            
+            Category? categoryFromDb = _db.Categories.Find(id); //only works with primary key
+            if (categoryFromDb == null)
+                return NotFound();
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj) 
+        {
             return View();
         }
     }
